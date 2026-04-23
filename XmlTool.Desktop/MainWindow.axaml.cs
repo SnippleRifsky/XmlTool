9667bd02270638702b97a70ee282ae94ade5ad5f
SnippleRifsky <shane_warren117@yahoo.com>
2026-04-23 09:02:54 -0400

    Initial commit

---
+++ XmlTool.Desktop/MainWindow.axaml.cs
@@ -0,0 +1,49 @@
+using System;
+using System.IO;
+using System.Xml.Linq;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
+
+namespace XmlTool.Desktop;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private XDocument _importDocument = new XDocument();
+
+    public async void OnLoadClick(object? sender, RoutedEventArgs evt)
+    {
+        try
+        {
+            // Get top level from the current control. Alternatively, you can use Window reference instead.
+            var topLevel = GetTopLevel(this);
+
+            // Start async operation to open the dialog.
+            if (topLevel == null) return;
+            var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+            {
+                Title = "Open Text File",
+                AllowMultiple = false
+            });
+
+            if (files.Count != 1) return;
+            // Open reading stream from the first file.
+            await using var stream = await files[0].OpenReadAsync();
+            using var streamReader = new StreamReader(stream);
+            // Reads all the content of file as a text.
+            var fileContent = await streamReader.ReadToEndAsync(); 
+            _importDocument = XDocument.Parse(fileContent);
+             
+            
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
