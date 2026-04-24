d518311d6fa8f1e9020ef7454439c4e0dbc8f09a
SnippleRifsky <shane_warren117@yahoo.com>
2026-04-24 00:46:43 -0400

    Update BacnetPoint.cs
    Made the members a nullable type.

--- XmlTool.Core/BacnetPoint.cs
+++ XmlTool.Core/BacnetPoint.cs
@@ -2,7 +2,7 @@
 
 public class BacnetPoint
 {
-    public string Name { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
-    public string Type { get; set; } = string.Empty;
+    public string? Name { get; set; } = string.Empty;
+    public string? Description { get; set; } = string.Empty;
+    public string? Type { get; set; } = string.Empty;
 }
