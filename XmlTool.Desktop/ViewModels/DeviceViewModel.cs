bc21ef5828f4f0179c43d77d7132b6b2f54bcee7
SnippleRifsky <shane_warren117@yahoo.com>
2026-04-24 19:35:54 -0400

    Create DeviceViewModel

---
+++ XmlTool.Desktop/ViewModels/DeviceViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using XmlTool.Core;
+
+namespace XmlTool.Desktop.ViewModels;
+
+public class DeviceViewModel
+{
+    public string DeviceName { get; set; }
+    public string DeviceDescription { get; set; }
+    public int  PointCount { get; set; }
+    public List<BacnetPoint> PointsList { get; set; }
+}
